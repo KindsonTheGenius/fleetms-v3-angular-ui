9c5120b9edd2beca6ea9948255ba5fd70ae06c7e
KindsonTheGenius <Smart24*>
2022-02-27 07:40:10 +0100

    Part 2 -Integrated AdminLTE Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    ContentWrapperComponent,
+    ControlSidebarComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
